d7091a1743c4f3763f7b49b40f1092d8b2c59661
Couson <couson@foxmail.com>
2019-01-09 23:21:45 -0800

    Update for no reasons

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var numPpl: UITextField!
+    @IBOutlet weak var totalAfterSplit: UILabel!
+    @IBOutlet weak var totalBeforeSplit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +28,25 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+
+//    @IBAction func split(_ sender: Any) {
+////        let one = Double(totalLabel.text!) ?? 0
+//        let total = Double(totalBeforeSplit.text!) ?? 100
+//        let num = Double(numPpl.text!) ?? 1
+//        let totalThen = total / num
+//
+//        totalAfterSplit.text = String(format: "$%.2f", totalThen)
+//    }
+//
     @IBAction func calculateTip(_ sender: AnyObject) {
         let tipRate = [0.15, 0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipRate[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        totalBeforeSplit.text = totalLabel.text
     }
     
     
